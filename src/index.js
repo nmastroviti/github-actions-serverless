922bf1f11982c5e6d4e86c1a027251d53b4f1072
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-08-31 00:49:51 -0300

    fix: pipeline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import serverlessExpress from '@vendia/serverless-express'
 import { app as listUsers } from './list'
 
-export const listUsersHandler = serverlessExpress({ listUsers })
+export const listUsersHandler = serverlessExpress({ app: listUsers })
