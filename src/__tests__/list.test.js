9cd54bc8f05d647baefd4fcfc1f30b23966d7c70
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-08-31 00:49:51 -0300

    fix: pipeline

--- src/__tests__/list.test.js
+++ src/__tests__/list.test.js
@@ -1,5 +1,5 @@
 import supertest from 'supertest'
-import { app as listUsers } from './list'
+import { app as listUsers } from '../list'
 
 const request = supertest(listUsers)
 
--- src/list.js
+++ src/list.js
@@ -2,11 +2,11 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', (req, res) => {
+app.get('/users', (req, res) =>
   res.json([
     { name: 'John Doe', email: 'johndoe@domain.com' },
     { name: 'Jane Doe', email: 'janedoe@domain.com' },
   ])
-})
+)
 
 export { app }
